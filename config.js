d1501a201c7676d75b4173521b6434cedecf3450
Luca <lk@ligero.one>
2025-11-30 11:38:25 +0100

    dodano config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyC7LMxmsx5hxQgYpRr6PGtPGoFJCQ6S_Ag",
+    authDomain: "wheel-decide-70f9c.firebaseapp.com",
+    databaseURL: "https://wheel-decide-70f9c-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "wheel-decide-70f9c",
+    storageBucket: "wheel-decide-70f9c.firebasestorage.app",
+    messagingSenderId: "570584620154",
+    appId: "1:570584620154:web:0ee1ac7a1944eb7922afc3"
+};
+
+// Adres Twojego backendu na Render.com
+export const API_URL = "https://wheel-kbe8.onrender.com";
